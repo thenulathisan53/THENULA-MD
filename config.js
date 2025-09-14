c17871da997c13ec001bc866def4258a21f31cfb
thenulathisan53 <thenulathisan53@gmail.com>
2025-09-14 17:58:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " sjsnssjshhshrhrbbb", // ඔයාගෙ session id එක
-MONGODB: process.env.MONGODB || "mongodb://mongo:sjjkkd",  //ඔයාගෙ mongoDb url එක
+SESSION_ID: process.env.SESSION_ID || "6EFxhBiA#wv6on1iwoO7unt4UXruMfOd7_wO3WbX0LFcghISe_to", // ඔයාගෙ session id එක
+MONGODB: process.env.MONGODB || "mongodb://mongo:gJCQBQJpLEfKkRVeuozBGKvUfjDfsmAP@shortline.proxy.rlwy.net:33187",  //ඔයාගෙ mongoDb url එක    
 };
  
